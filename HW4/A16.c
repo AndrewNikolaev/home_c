a3b994a017337fa42b665afabdd44b05ad8cc2c9
KRYPTONITE\a.nikolaev <a.nikolaev@kryptonite.ru>
2026-05-24 23:39:40 +0300

    Выгрузка дз4

---
+++ HW4/A16.c
@@ -0,0 +1,37 @@
+/*
+ * A6.c
+ * 
+ * Copyright 2026 a.nikolaev <a.nikolaev@TAG-5736>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c );
+	if (a<b&&b<c&&a<c)
+		printf("YES");
+	else 
+		printf("NO");
+	return 0;
+}
+
